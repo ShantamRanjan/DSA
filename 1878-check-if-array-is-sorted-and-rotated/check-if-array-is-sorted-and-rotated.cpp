14c7c29384eb0f8739f37d69a340c7ecd3b66a1c
ShantamRanjan <142114496+ShantamRanjan@users.noreply.github.com>
2025-09-20 19:18:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (13.33%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        // If there is at most one drop, it's sorted and rotated
+        return count <= 1;
+    }
+};
